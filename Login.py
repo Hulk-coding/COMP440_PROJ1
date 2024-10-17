eb4acb82466747ac9cdbe5255954fc1e65e268d8
Hashmat <hashmath2003@gmail.com>
2024-10-17 13:10:18 -0700

    updating my local repo

--- Login.py
+++ Login.py
@@ -1,12 +1,22 @@
-from PyQt5.QtWidgets import QLabel, QLineEdit, QPushButton, QWidget, QVBoxLayout, QFormLayout, QHBoxLayout
+import bcrypt
+from PyQt5.QtWidgets import (
+    QLabel,
+    QLineEdit,
+    QPushButton,
+    QWidget,
+    QVBoxLayout,
+    QFormLayout,
+    QHBoxLayout,
+)
 from PyQt5.QtCore import Qt
 from Create import Create
+
 
 class Login(QWidget):
     def __init__(self):
         super().__init__()
-        
-        #labels for input
+
+        # labels for input
         userName = QLabel("Username")
         userPassword = QLabel("Password")
 
@@ -14,19 +24,18 @@ class Login(QWidget):
         userPasswordIn = QLineEdit()
         userPasswordIn.setEchoMode(QLineEdit.Password)
 
-
-        #buttons for login page - greg edited
+        # buttons for login page - greg edited
         self.loginButton = QPushButton("Login", self)
         self.createAccountButton = QPushButton("Create Account", self)
-        
+
         # greg added function
         self.createAccountButton.clicked.connect(self.open_create_window)
-        
-        #formatting page, creating layouts for the components 
+
+        # formatting page, creating layouts for the components
         pageLayout = QFormLayout()
         pageLayout.addRow(userName, userNameIn)
         pageLayout.addRow(userPassword, userPasswordIn)
-        
+
         buttonsLayout = QHBoxLayout()
 
         # greg edit - add self
@@ -38,15 +47,11 @@ class Login(QWidget):
         # self.setStyleSheet("QWidget { border: 2px solid black; padding; 10px;") # this is not doing anything visible
 
         pageLayout.setAlignment(Qt.AlignCenter)
-        
+
         self.setLayout(pageLayout)
         # self.setWindowTitle("Login")
-        
+
     # greg added create function
     def open_create_window(self):
         self.create_window = Create()
         self.create_window.show()
-    
-        
-       
-        
